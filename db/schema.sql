f0170f546040fd4a781d75b309a332ade87932d5
Tabitha <spencersalmon.tabitha@gmail.com>
2022-08-24 10:15:22 -0400

    fixed back

--- db/schema.sql
+++ db/schema.sql
@@ -31,9 +31,9 @@ CREATE TABLE employee (
   FOREIGN KEY (role_id)
   REFERENCES role(id)
   ON DELETE SET NULL
-  FOREIGN KEY (manager_id)
-  REFERENCES employee(id)
-  ON DELETE SET NULL
+  -- FOREIGN KEY (manager_id)
+  -- REFERENCES employee(id)
+  -- ON DELETE SET NULL
 );
 
 DESCRIBE department;
